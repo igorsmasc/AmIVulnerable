f1a87f670f169d98a2b61df456f0a5c121841bef
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2021-03-31 02:30:05 -0300

    Initial Commit

---
+++ src/main/java/com/igormascarenhas/amivulnerable/AmivulnerableApplication.java
@@ -0,0 +1,13 @@
+package com.igormascarenhas.amivulnerable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmivulnerableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmivulnerableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igormascarenhas/amivulnerable/AmivulnerableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igormascarenhas.amivulnerable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmivulnerableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
