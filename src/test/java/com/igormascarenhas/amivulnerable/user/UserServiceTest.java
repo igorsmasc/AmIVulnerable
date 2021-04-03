c63d7890aabaec7dc34922e8a687c39ad919c4dc
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2021-04-03 18:17:46 -0300

    Add test - willThrowWhenEmailIsTaken

--- src/test/java/com/igormascarenhas/amivulnerable/user/UserServiceTest.java
+++ src/test/java/com/igormascarenhas/amivulnerable/user/UserServiceTest.java
@@ -8,7 +8,16 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.mockito.Mockito.verify;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
+import static org.mockito.BDDMockito.given;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -54,6 +63,30 @@ class UserServiceTest {
 
         assertThat(userCaptured).isEqualTo(user);
 
+    }
+
+    @Test
+    void willThrowWhenEmailIsTaken() {
+        // Given
+        User user = new User(
+                "igor@mail.com",
+                "123",
+                "Igor Santos Mascarenhas"
+        );
+
+        Optional<User> userOptional = Optional.of(user);
+
+        given(userRepository.findUserByEmail(anyString()))
+                .willReturn(userOptional);
+
+        // When
+        // Then
+        assertThatThrownBy(() ->
+                underTest.addNewUser(user))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("This emails is already in use.");
+
+        verify(userRepository, never()).save(any());
     }
 
     @Test
