60929cd09a98da757579f1986fec12e13d931c22
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2021-04-03 02:55:55 -0300

    Add Test - itShouldFindUserByEmail

--- src/test/java/com/igormascarenhas/amivulnerable/user/UserRepositoryTest.java
+++ src/test/java/com/igormascarenhas/amivulnerable/user/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.igormascarenhas.amivulnerable.user;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,16 +14,20 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
     @Test
     void itShouldFindUserByEmail() {
-        // Given String email, String password, String name
+        // Given
         String email = "igor@mail.com";
         User user = new User(
                 email,
                 "123",
                 "Igor Santos Mascarenhas"
         );
-
         userRepository.save(user);
 
         // When
@@ -29,6 +35,17 @@ class UserRepositoryTest {
 
         // Then
         assertThat(expected).isTrue();
+    }
 
+    @Test
+    void itShouldNotFindUserByEmail() {
+        // Given
+        String email = "notfound@mail.com";
+
+        // When
+        boolean expected = userRepository.findUserByEmail(email).isPresent();
+
+        // Then
+        assertThat(expected).isFalse();
     }
 }
