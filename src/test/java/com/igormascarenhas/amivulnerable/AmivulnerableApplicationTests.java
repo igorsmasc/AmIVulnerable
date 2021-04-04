2a302b2a39ab9000edf310acf35dfb44d635453f
igorsmasc <igorsmascarenhas@outlook.com>
2021-04-04 15:26:59 -0700

    disable test - contextLoads

--- src/test/java/com/igormascarenhas/amivulnerable/AmivulnerableApplicationTests.java
+++ src/test/java/com/igormascarenhas/amivulnerable/AmivulnerableApplicationTests.java
@@ -1,5 +1,6 @@
 package com.igormascarenhas.amivulnerable;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AmivulnerableApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
